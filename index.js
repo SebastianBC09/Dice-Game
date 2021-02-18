5b661562c905d200b950b178f494b5fe17a25dd0
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-02-18 17:12:42 -0500

    JS File with random number created & used to set attribute to change the dice image. Working perfectly, checked random number 0.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-var randomNumber1 = Math.random()
-randomNumber1 = randomNumber1 * 7
-randomNumber1 = Math.floor(randomNumber1)
+var randomNumber1 = Math.floor(Math.random() * 7)
+console.log(randomNumber1)
+var randomNumber2 = Math.floor(Math.random() * 7)
+console.log(randomNumber2)
+
+var image1 = document.querySelectorAll("img")[0].setAttribute("src","images/dice" + randomNumber1 + ".png")
