19cb706c249eb60120a0e89eba03c339786112d4
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-02-18 17:27:19 -0500

    Random number created & added to second image. Working good.

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ var randomNumber2 = Math.floor(Math.random() * 7)
 console.log(randomNumber2)
 
 var image1 = document.querySelectorAll("img")[0].setAttribute("src","images/dice" + randomNumber1 + ".png")
+var image2 = document.querySelectorAll("img")[1].setAttribute("src","images/dice" + randomNumber2 + ".png")
