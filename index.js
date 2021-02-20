7c518403bf95d1daf1d96c4673ed0ea156aeb988
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-02-20 15:59:35 -0500

    If statement to declare the winner of the game or draw has been created & working. Random Number issue regarding number 0 has been fixed.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-var randomNumber1 = Math.floor(Math.random() * 7)
+var randomNumber1 = Math.floor((Math.random() * 6) + 1)
 console.log(randomNumber1)
-var randomNumber2 = Math.floor(Math.random() * 7)
+var randomNumber2 = Math.floor((Math.random() * 6) + 1)
 console.log(randomNumber2)
 
 var image1 = document.querySelectorAll("img")[0].setAttribute("src","images/dice" + randomNumber1 + ".png")
 var image2 = document.querySelectorAll("img")[1].setAttribute("src","images/dice" + randomNumber2 + ".png")
+
+if (randomNumber1 > randomNumber2) {
+  var title =  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!"
+} else if (randomNumber2 > randomNumber1) {
+  var title = document.querySelector("h1").innerHTML = "Player 2 Wins!🚩"
+} else {
+  var title = document.querySelector("h1").innerHTML = "Draw!"
+}
